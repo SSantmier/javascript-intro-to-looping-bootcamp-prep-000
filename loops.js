72b86784f981574f5feb11bbc21a0684e7a048d9
Sara S <ssantmier@gmail.com>
2018-06-27 18:02:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,1 +1,1 @@
-forLoop (let i = 0; i < t; i++) {if i < 1 console.log("I am 1 strange loop." else console.log("I am ${i} strange loops.")}
+forLoop (i = 0; i < t; i++) {if i < 1 {return console.log("I am 1 strange loop.")} else return {console.log("I am ${i} strange loops.")}}
